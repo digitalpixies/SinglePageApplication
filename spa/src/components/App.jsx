edb512d58fc91ccee7578119729d5c1636ff8e2b
Robert MBP Huie <rhuie@digitalpixies.com>
2017-09-05 11:43:33 -0700

    Framework7 version basic table

--- spa/src/components/App.jsx
+++ spa/src/components/App.jsx
@@ -1,198 +1,82 @@
 import React, {PropTypes} from 'react';
-
+/*
 import {
-	Framework7App, Statusbar, Panel, View, Navbar, Pages, Page, ContentBlock, ContentBlockTitle, 
+	Framework7App, Statusbar, Panel, View, Navbar, Pages, Page, ContentBlock, ContentBlockTitle,
 	List, ListItem, Views, NavLeft, Link, NavCenter, NavRight, GridRow, GridCol, Button, Popup,
 	LoginScreen, LoginScreenTitle, ListButton, ListLabel, FormLabel, FormInput
 } from 'framework7-react';
+*/
+import {Framework7App, Pages, Page, View, Views} from 'framework7-react';
 
 import {routes} from '../routes';
 
-const LeftPanel = (props, context) => (
-	<Panel left reveal layout="dark">
-		<View id="left-panel-view" navbarThrough dynamicNavbar="true">
-			{context.framework7AppContext.theme.ios ? <Navbar title="Left Panel"></Navbar> : null}
-			<Pages>
-				<Page>
-					{context.framework7AppContext.theme.material ? <Navbar title="Left Panel"></Navbar> : null}
-					<ContentBlock inner>
-						<p>Left panel content goes here</p>
-					</ContentBlock>
-					<ContentBlockTitle>Load page in panel</ContentBlockTitle>
-					<List>
-						<ListItem link="/about/" title="About"></ListItem>
-						<ListItem link="/form/" title="Form"></ListItem>
-					</List>
-					<ContentBlockTitle>Load page in main view</ContentBlockTitle>
-					<List>
-						<ListItem link="/about/" title="About" linkView="#main-view" linkClosePanel></ListItem>
-						<ListItem link="/form/" title="Form" linkView="#main-view" linkClosePanel></ListItem>
-					</List>
-				</Page>
-			</Pages>
-		</View>
-	</Panel>
-);
-
-LeftPanel.contextTypes = {
-	framework7AppContext: PropTypes.object
-};
-
-const RightPanel = (props, context) => (
-	<Panel right cover layout="dark">
-		<View id="right-panel-view" navbarThrough dynamicNavbar={true}>
-			{context.framework7AppContext.theme.ios ? <Navbar title="Right Panel" sliding /> : null}
-			<Pages>
-				<Page>
-					{context.framework7AppContext.theme.material ? <Navbar title="Right Panel" sliding /> : null}
-					<ContentBlock>
-						<p>Right panel content goes here</p>
-					</ContentBlock>
-					<ContentBlockTitle>Load page in panel</ContentBlockTitle>
-					<List>
-						<ListItem link="/about/" title="About"></ListItem>
-						<ListItem link="/form/" title="Form"></ListItem>
-					</List>
-					<ContentBlockTitle>Load page in main view</ContentBlockTitle>
-					<List>
-						<ListItem link="/about/" title="About" linkView="#main-view" linkClosePanel></ListItem>
-						<ListItem link="/form/" title="Form" linkView="#main-view" linkClosePanel></ListItem>
-					</List>
-				</Page>
-			</Pages>
-		</View>
-	</Panel>
-);
-
-RightPanel.contextTypes = {
-	framework7AppContext: PropTypes.object
-};
-
 const MainViews = (props, context) => {
-	return (
-		<Views>
-			<View id="main-view" navbarThrough dynamicNavbar={true} main url="/">
-				{/* Navbar */}
-				{context.framework7AppContext.theme.ios ? (
-					<Navbar>
-						<NavLeft>
-							<Link icon="icon-bars" openPanel="left" />
-						</NavLeft>
-						<NavCenter sliding>Framework7</NavCenter>
-						<NavRight>
-							<Link icon="icon-bars" openPanel="right"></Link>
-						</NavRight>
-					</Navbar>
-				) : null}
-				{/* Pages */}
-				<Pages>
-					<Page>
-						{context.framework7AppContext.theme.material ? (
-							<Navbar>
-								<NavLeft>
-									<Link icon="icon-bars" openPanel="left" />
-								</NavLeft>
-								<NavCenter sliding>Framework7</NavCenter>
-								<NavRight>
-									<Link icon="icon-bars" openPanel="right"></Link>
-								</NavRight>
-							</Navbar>
-						) : null}						
-						<ContentBlockTitle>Welcome to my App</ContentBlockTitle>
-						<ContentBlock inner>
-							<p>Duis sed erat ac eros ultrices pharetra id ut tellus. Praesent rhoncus enim ornare ipsum aliquet ultricies. Pellentesque sodales erat quis elementum sagittis.</p>
-						</ContentBlock>
-						<ContentBlockTitle>Navigation</ContentBlockTitle>
-						<List>
-							<ListItem link="/about/" title="About"></ListItem>
-							<ListItem link="/form/" title="Form"></ListItem>							
-						</List>
-						<ContentBlockTitle>Side Panels</ContentBlockTitle>
-						<ContentBlock>
-							<GridRow>
-								<GridCol width={50}>
-									<Button openPanel="left">Left Panel</Button>
-								</GridCol>
-								<GridCol width={50}>
-									<Button openPanel="right">Right Panel</Button>
-								</GridCol>
-							</GridRow>
-						</ContentBlock>
-						<ContentBlockTitle>Modals</ContentBlockTitle>
-						<ContentBlock>
-							<GridRow>
-								<GridCol width={50}>
-									<Button openPopup="#popup">Popup</Button>
-								</GridCol>
-								<GridCol width={50}>
-									<Button openLoginScreen="#login-screen">Login Screen</Button>
-								</GridCol>
-							</GridRow>
-						</ContentBlock>
-					</Page>
-				</Pages>
-			</View>
-		</Views>
-	);
+  var contacts = [
+    {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe"
+    }, {
+      id: 2,
+      first_name: "Jane",
+      last_name: "Doe"
+    }
+  ];
+  let header = {
+    id: "ID",
+    first_name: "First Name",
+    last_name: "Last Name"
+  };
+  let HeaderRow = function(row) {
+    return (
+      <tr key={row.id}>
+        <th className="label-cell">{row.id}</th>
+        <th className="label-cell">{row.first_name}</th>
+        <th className="tablet-only">{row.last_name}</th>
+      </tr>
+    );
+  };
+  let DataRow = function(row) {
+    return (
+      <tr key={row.id}>
+        <td className="label-cell">{row.id}</td>
+        <td className="label-cell">{row.first_name}</td>
+        <td className="tablet-only">{row.last_name}</td>
+      </tr>
+    );
+  };
+  let dataRows = contacts.map(DataRow);
+  return (
+    <Views>
+      <View id="main-view" main url="/">
+        {/* Navbar */}
+        {/* Pages */}
+        <Pages>
+          <Page>
+            <div className="data-table">
+              <table>
+                <thead>
+                  {HeaderRow(header)}
+                </thead>
+                <tbody>
+                  {dataRows}
+                </tbody>
+              </table>
+            </div>
+          </Page>
+        </Pages>
+      </View>
+    </Views>
+  );
 };
 
 MainViews.contextTypes = {
-	framework7AppContext: PropTypes.object
+  framework7AppContext: PropTypes.object
 };
 
-const AppPopup = () => (
-	<Popup id="popup">
-		<View navbarFixed>
-			<Pages>
-				<Page>
-					<Navbar title="Popup">
-						<NavRight>
-							<Link closePopup>Close</Link>
-						</NavRight>
-					</Navbar>
-					<ContentBlock>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto. Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis, a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati, quod.</ContentBlock>
-				</Page>
-			</Pages>
-		</View>
-	</Popup> 
-);
-
-const AppLoginScreen = () => (
-	<LoginScreen id="login-screen">
-		<View>
-			<Pages>
-				<Page loginScreen>
-					<LoginScreenTitle>Login</LoginScreenTitle>
-					<List form>
-						<ListItem>
-							<FormLabel>Username</FormLabel>
-							<FormInput name="username" placeholder="Username" type="text" />
-						</ListItem>
-						<ListItem>
-							<FormLabel>Password</FormLabel>
-							<FormInput name="password" type="password" placeholder="Password" />
-						</ListItem>
-					</List>
-					<List>
-						<ListButton title="Sign In" closeLoginScreen />
-						<ListLabel>
-							<p>Click Sign In to close Login Screen</p>
-						</ListLabel>
-					</List>
-				</Page>
-			</Pages>
-		</View>
-	</LoginScreen>
-);
-
-export const App = () => (	
-	//Change themeType to "material" to use the Material theme
-	<Framework7App themeType="ios" routes={routes}>		
-		<Statusbar />		
-		<LeftPanel />
-		<RightPanel />
-		<MainViews />
-		<AppPopup />
-		<AppLoginScreen />
-	</Framework7App>  
+//Change themeType to "material" to use the Material theme
+export const App = () => (
+  <Framework7App themeType="ios" routes={routes}>
+    <MainViews/>
+  </Framework7App>
 );
--- spa/src/routes.js
+++ spa/src/routes.js
@@ -1,10 +1,1 @@
-import {About} from './components/pages/About';
-import {Form} from './components/pages/Form';
-
-export const routes = [{
-    path: '/about/',
-    component: About
-}, {
-    path: '/form/',
-    component: Form
-}];
+export const routes = [];
